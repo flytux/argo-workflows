76a3bd4e35a969aee1957d20bd10a6cacc504583
Jaehoon <hooney.jung@gmail.com>
2023-05-15 11:01:37 +0900

    Update setup-argo.sh

--- scripts/setup-argo.sh
+++ scripts/setup-argo.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-
-while [[ $(kubectl get crd | grep ingressroutes.traefik.containo.us | wc -l) == 0 ]];
-do
-  sleep 5
-done;
+export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
 
 kubectl create ns argo
 kubectl create ns argo-events
 kubectl create ns go-api
 
-helm upgrade --install atlantbh charts/argo-workflows --namespace argo --values charts/argo-workflows/argo-values.yaml
-helm upgrade --install atlantbh charts/argo-events --namespace argo-events --values charts/argo-events/argo-values.yaml
+helm upgrade --install argo-workflow charts/argo-workflows --namespace argo --values charts/argo-workflows/argo-values.yaml
+helm upgrade --install argo-event charts/argo-events --namespace argo-events --values charts/argo-events/argo-values.yaml
 
 kubectl apply -f scripts/resources/raw/yaml/setup/ingresses.yaml
 kubectl apply -f scripts/resources/raw/yaml/argo-workflows/service-account-workflow-executor.yaml
